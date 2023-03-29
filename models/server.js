4ff62ba602c47a269ff57a185f33ab037961bd31
Stalin Huallullo <antonyhuallullo@gmail.com>
2023-03-28 21:09:02 -0500

    deploy in Heroku

--- models/server.js
+++ models/server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const socketio = require("socket.io");
 const path = require("path");
 const Sockets = require("./sockets");
-
+const cors     = require('cors');
 
 class Server {
     constructor(){
@@ -20,6 +20,8 @@ class Server {
 
     middlewares() {
         this.app.use(express.static(path.resolve(__dirname,"../public")))
+        // CORS
+        this.app.use( cors() );
     }
 
     configurarSockets(){
